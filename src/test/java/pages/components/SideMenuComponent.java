46367f320e968bc14d14fb656b89748a9ff3d805
Nechaeva Natalya <nechaevanatascha@yandex.ru>
2025-03-27 22:25:55 +0300

    Fixed tests

--- src/test/java/pages/components/SideMenuComponent.java
+++ src/test/java/pages/components/SideMenuComponent.java
@@ -12,7 +12,9 @@ public class SideMenuComponent {
     private final ElementsCollection sideMenuComponents = $$("span[class*='CatalogTreeSectionCard_name']");
     private final List<String> elements = List.of("Собрали для вас", "От Самоката", "Готовая еда", "Овощи и фрукты",
             "Молоко, яйца и сыр", "Хлеб и выпечка", "Мясо и рыба", "Вода и напитки", "Сладкое", "Снеки", "Бакалея", "Морозилка",
-            "ЗОЖ и вегетарианство", "Для детей", "Для животных", "Красота и здоровье", "Для дома");
+            "ЗОЖ и вегетарианство", "Для детей", "Красота и здоровье", "Одежда, обувь и аксессуары",
+            "Для животных", "Всё для дома", "Канцелярия, хобби и спорт", "Техника и электроника", "Для автомобиля");
+
 
     public void checkSideMenu() {
         sideMenuComponents.shouldHave(CollectionCondition.textsInAnyOrder(elements));
